ef2969c134ff851fbec95dcd2ebdb202de8aa3fe
Andre Viana <code.andre3@gmail.com>
2021-07-09 16:41:36 -0300

    add html with components and services

--- src/app/conversor/components/conversor.component.ts
+++ src/app/conversor/components/conversor.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { MoedaService } from './../services/moeda.service';
+import { ConversorService } from '../services/conversor.service';
+import { Conversao } from '../models/conversao.model';
+import { ConversaoResponse } from '../models/conversao-response.model';
+import { Moeda } from '../models/moeda.model';
 
 @Component({
   selector: 'app-conversor',
@@ -7,9 +15,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConversorComponent implements OnInit {
 
-  constructor() { }
+  moedas: Moeda[];
+  conversao: Conversao;
+  possuiErro: boolean;
+  conversaoResponse: ConversaoResponse; 
+
+  @ViewChild("conversaoForm", { static: true }) conversaoForm: NgForm;
+
+  constructor(
+    private moedaService: MoedaService,
+    private conversorService: ConversorService
+  ) { }
 
   ngOnInit(): void {
+    this.moedas = this.moedaService.listarTodos();
+    this.init();
   }
 
+  init() { //incializa o nosso código
+    this.conversao = new Conversao('USD', 'BRL', null);  //chama conversao, new Conversao 
+    this.possuiErro = false;
+  }
+
+  converter() {
+    if (this.conversaoForm.form.valid) {
+      alert('Convertendo' + JSON.stringify(this.conversao))
+    }
+  }
 }
--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { ConversorComponent } from './components/conversor.component';
 import { MoedaService } from './services/moeda.service';
@@ -12,7 +13,8 @@ import { ConversorService } from './services/conversor.service';
     ConversorComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   exports: [
     ConversorComponent
