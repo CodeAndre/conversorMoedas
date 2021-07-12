aff3225037e3ab111e0f51ce6843f8370bce995f
Andre Viana <code.andre3@gmail.com>
2021-07-11 22:23:46 -0300

    final commit :tada:

--- src/app/conversor/pipes/data-br.pipe.ts
+++ src/app/conversor/pipes/data-br.pipe.ts
@@ -5,8 +5,17 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DataBrPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(dataEn: string): string {
+    if(!dataEn) {
+      return ''
+    }
+
+    const dataArray = dataEn.split('-'); //separar ano(0) - mes(1) - dia (2)
+    if(dataArray.length !== 3) {
+      return dataEn;
+    }
+
+    return dataArray[2] + '/' + dataArray[1] + '/' + dataArray[0]; // dia/mes/ano
   }
 
 }
